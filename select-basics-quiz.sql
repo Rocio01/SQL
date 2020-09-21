406548935be095365c8322223486276d08a5751f
Rocio01 <zrmartinezg@gmail.com>
2020-09-21 10:41:50 -0500

    Add bbc-quiz

--- select-basics-quiz.sql
+++ select-basics-quiz.sql
@@ -1,27 +1,32 @@
- "SELECT Quiz from sqlzoo"
+--  SELECT Quiz from sqlzoo
 
-Answers 
+-- Answers 
 
--1  SELECT name, population FROM world WHERE population BETWEEN 1000000 AND 1250000
+--1  
+SELECT name, population FROM world WHERE population BETWEEN 1000000 AND 1250000
 
--2   "Table-E"
+--2  
+     "Table-E"
     Albania	3200000
     Algeria	32900000
 
--3 SELECT name FROM world  WHERE name LIKE '%a' OR name LIKE '%l'
+--3 
+SELECT name FROM world  WHERE name LIKE '%a' OR name LIKE '%l'
 
--4
+--4
     "name	length(name)"
     Italy	5
     Malta	5
     Spain	5
 
--5
+--5
     Andorra	936
 
 
--6  SELECT name, area, population  FROM world WHERE area > 50000 AND population < 10000000
+--6  
+SELECT name, area, population  FROM world WHERE area > 50000 AND population < 10000000
 
 
--7 SELECT name, population/area   FROM world WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
+--7 
+SELECT name, population/area   FROM world WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
 
